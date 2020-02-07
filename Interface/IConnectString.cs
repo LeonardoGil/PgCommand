f4316685b508826185f36457f0233488a3c33838
Leonardo Albuquerque Gil <leonardoalbuquerquegil@hotmail.com>
2020-02-06 21:09:33 -0300

    Adicionar arquivos de projeto.

---
+++ Interface/IConnectString.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PgCommand.Interface
+{
+    interface IConnectString
+    {
+        string Host { get; set; }
+        string Name { get; set; }
+        MD5 Password { get; set; } 
+        int Port { get; set; }
+    }
+}
